25f7d7952575c8f2664e4f636858582113603488
Daniel Eric Marasco <daniel.eric.marasco@gmail.com>
2020-06-07 11:24:48 -0400

    update geojson demo

--- src/locdata/dataset/base.py
+++ src/locdata/dataset/base.py
@@ -1,8 +1,25 @@
 
+import pandas as pd
+from h3 import h3
+
 class SpatialDataset():
     """
     """
-    def __init__(self, source=""):
+    def __init__(self, source, h3_resolution, ring_distance):
         self.name = "Abstract Spatial Dataset"
         self.source=source
+        self.h3_resolution = h3_resolution
+        self.ring_distance = ring_distance
+        self.df = pd.DataFrame()
 
+    def drop_prefix(self, prefix):
+        print("Before: ",self.df.columns)
+        self.df.columns = self.df.columns.str.replace(prefix,"")
+        print("After: ", self.df.columns)
+
+    def index_df(self):
+        """
+        add h3 index to dataset
+        """
+        self.df[f'hex_id{self.h3_resolution}'] =self.df.apply(lambda row: h3.geo_to_h3(row["latitude"], row["longitude"],
+                                               self.h3_resolution), axis=1)
--- src/locdata/dataset/geojson.py
+++ src/locdata/dataset/geojson.py
@@ -1,45 +1,71 @@
+"""
+Module containing GeoJson Dataset
+"""
 import json
+from collections import defaultdict
 import pandas as pd
 from pandas.io.json import json_normalize
+from h3 import h3
 
 from .base import SpatialDataset
 
 class GeoJsonDataset(SpatialDataset):
-    def __init__(self, source, properties=[]):
-        super().__init__(source)
-        print(self.source)
-        if not len(properties):
-            self.properties = ['name','amenity','cuisine',
+    """
+    Class for GeoJson Datasets
+    """
+    def __init__(self, source, properties=None, filter_values=None, h3_resolution=11,
+                 ring_distance=2, index_col='id'):
+        super().__init__(source, h3_resolution, ring_distance)
+        if properties is None:
+            self.properties = ['name', 'amenity','cuisine',
                                'addr:housenumber',
-                               'addr:street','addr:city',
-                               'addr:state','addr:postcode']
+                               'addr:street', 'addr:city',
+                               'addr:state', 'addr:postcode']
         else:
             self.properties = properties
-        self.df = self._read_geojson()
+
+        if filter_values is None:
+            self.filter_values={}
+        else:
+            self.filter_values = filter_values
+
+        self.index_col = index_col
 
 
-    def _read_geojson(self):
+        self.read_geojson()
+
+
+    def read_geojson(self):
         '''
         Read a GeoJSON file into a dataframe and drop rows with
         null geometry. Extract the latitude and longitude as
         separate columns from the geometry's coordinates
         '''
-    
+
         with open(self.source) as f:
             stops_geodata = json.load(f)
-        
-        df = pd.DataFrame(json_normalize(stops_geodata['features']))
-        n_rows_orig = df.shape[0]
-        
-        df.dropna(subset=["geometry.coordinates"], inplace = True, axis = 0)
-        n_rows_clean = df.shape[0]
-        print("Cleaning null geometries, eliminated ", n_rows_orig - n_rows_clean, 
-            " rows out of the original ",n_rows_orig, " rows")
-        
-        df['longitude'] = df["geometry.coordinates"].apply(lambda x: x[0]) 
-        df['latitude'] = df["geometry.coordinates"].apply(lambda x: x[1]) 
-        
-        selection = (['id', 'latitude', 'longitude'] +
-            [f"properties.{p}" for p in self.properties])
-        df = df[selection]
-        return df
+
+        self.df = pd.DataFrame(json_normalize(stops_geodata['features']))
+        self.drop_prefix("properties.")
+
+        n_rows_orig = self.df.shape[0]
+
+        self.df.dropna(subset=["geometry.coordinates"], inplace=True, axis=0)
+        n_rows_clean = self.df.shape[0]
+        print("Cleaning null geometries, eliminated ", n_rows_orig - n_rows_clean,
+              " rows out of the original ", n_rows_orig, " rows")
+
+        self.df['longitude'] = self.df["geometry.coordinates"].apply(lambda x: x[0])
+        self.df['latitude'] = self.df["geometry.coordinates"].apply(lambda x: x[1])
+
+        print(self.df.columns)
+        # Remove irrelevant columns
+        selection = (['id', 'latitude', 'longitude'] + self.properties)
+        self.df = self.df[selection]
+
+        # Filter records that don't match `filter_values`
+        for k, v in self.filter_values.items():
+            self.df = self.df.loc[self.df[k].isin(v), :]
+
+
+
