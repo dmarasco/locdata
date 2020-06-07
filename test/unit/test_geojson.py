13935b45b538069d8acca300e96126755ab3748d
Daniel Eric Marasco <daniel.eric.marasco@gmail.com>
2020-06-07 11:37:07 -0400

    add initial test

---
+++ test/unit/test_geojson.py
@@ -0,0 +1,15 @@
+from locdata.dataset.geojson import GeoJsonDataset
+
+SOURCE = "data/osm_nodes_amnenities_dc_bbox.geojson"
+CENTER = {"latitude": 38.882843, "longitude":-76.997509}
+FILTER_VALUES = {"amenity":["restaurant", "fast_food", "cafe", 'bar', "pub"]}
+
+
+def test_load_geojson():
+    """
+    Test to confirm GeoJson can load and be read properly
+    """
+    gds = GeoJsonDataset(SOURCE, filter_values=FILTER_VALUES)
+    amenities_count = gds.df['amenity'].value_counts().sort_values(ascending=False)
+    assert amenities_count['restaurant'] == 963
+    return 0
